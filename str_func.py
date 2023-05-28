304a52f37c5a3b8bba3f71a3cd860ce03eb177b1
Yura <Orl0ff.1ur@yandex.ru>
2023-05-28 23:10:45 +0300

    add upper()

--- str_func.py
+++ str_func.py
@@ -2,3 +2,7 @@ def str_func(value):
     """ все буквы заглавные
     docstring"""
     return value.upper()
+
+def str_func1(value):
+    """ каждое слово в строке начинается с заглавной буквы"""
+    return value.title()
