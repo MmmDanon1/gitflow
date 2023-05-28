114fe8033425c14d9ad5533b798116e55ea5bfa0
Yura <Orl0ff.1ur@yandex.ru>
2023-05-28 23:16:30 +0300

    add upper, title

--- str_func.py
+++ str_func.py
@@ -1,8 +1,7 @@
 def str_func(value):
-    """ все буквы заглавные
-    docstring"""
+    """ """
     return value.upper()
 
 def str_func1(value):
-    """ каждое слово с заглавной буквы"""
+    """ """
     return value.title()
